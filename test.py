75859bff7b35c2b0f5e3d00a496a812a903e212f
RedAlert06 <RedAlert06@gmail.com>
2022-02-13 19:44:37 +0300

    reset to 5d30392 and commit it

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+from ssl import SSLSession, SSLSocket
+
+
 print('Hello World!')
 #file created 15.01.22 8:30
 print('This line typed at 13:18 25.01.2022')
+#test of ignore
+#$$$$$$$$$$$$
+print("HELLOWWW WWORLD! ")
+A = 1 + 2
+print(A)
+----
