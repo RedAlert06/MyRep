2f8dbd59cbc9bb09752ec99541812b9b4dc39195
RedAlert06 <RedAlert06@gmail.com>
2022-02-13 20:51:06 +0300

    conflict branch

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from ssl import SSLSession, SSLSocket
 
-#feature1 another line
+#feature1 another line conflict_branch
 print('Hello World!')
 #file created 15.01.22 8:30
 print('This line typed at 13:18 25.01.2022')
