7ccca480a9c36baf2b7da1edeae2d160713025df
RedAlert06 <RedAlert06@gmail.com>
2022-02-17 17:55:56 +0300

    Created sample.xml

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 from ssl import SSLSession, SSLSocket
 
-<<<<<<< HEAD
 #feature1 another line conflict1 main
-=======
 #feature1 another line conflict_branch
->>>>>>> feature1
 print('Hello World!')
 #file created 15.01.22 8:30
 print('This line typed at 13:18 25.01.2022')
